da033ce26f21bd836d8ef47de5fa245a4acfc71e
Priyav Shah <priyavrocks@gmail.com>
2017-01-16 13:03:50 +0000

    Removed typings files

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/react-native/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
-/// <reference path="modules/moment/index.d.ts" />
-/// <reference path="modules/react-native-router-flux/index.d.ts" />
-/// <reference path="modules/react-redux/index.d.ts" />
-/// <reference path="modules/seamless-immutable/index.d.ts" />
